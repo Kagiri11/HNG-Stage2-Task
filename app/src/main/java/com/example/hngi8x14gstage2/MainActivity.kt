447b69e8c08a28261d4ef8bb65e18c022e680093
Kagiri11 <charlesmaish423@gmail.com>
2021-08-20 20:30:43 +0300

    used databinding to provide views

--- app/src/main/java/com/example/hngi8x14gstage2/MainActivity.kt
+++ app/src/main/java/com/example/hngi8x14gstage2/MainActivity.kt
@@ -9,15 +9,19 @@ import androidx.databinding.DataBindingUtil
 import com.example.hngi8x14gstage2.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    //Calling upon the binding class
+    lateinit var binding:ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        val editTextOne = findViewById<EditText>(R.id.etOne)
-        val textViewOne = findViewById<TextView>(R.id.tvOne)
-        val btnOne = findViewById<Button>(R.id.btnOne)
-        btnOne.setOnClickListener {
-            val userContent = editTextOne.text.toString()
-            textViewOne.text=userContent
+        //the binding class will now provide us with the views
+        binding= ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.apply {
+            btnOne.setOnClickListener {
+                val userContent = etOne.text.toString()
+                tvOne.text=userContent
+            }
         }
     }
 }
---
+++ readme.md
@@ -0,0 +1,7 @@
+# My HNGi8 stage 2 task
+The answer to question 2 is in the src -> main-> java->com ->example->hngi8x14Gstage2 
+it is in a file called Question2Task.kt
+
+The solution to question 5 is in the mainactivity class
+
+## The following is the apps gif
